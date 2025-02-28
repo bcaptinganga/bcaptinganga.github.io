4c8dd4a032f9a58c8e81accb1746a051f1df4975
Our Lady Of The Holy Rosary Ting'ang'a <160110899+bcaptinganga@users.noreply.github.com>
2025-02-28 16:45:02 +0300

    Update readings.js

---
+++ readings.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+  const navbarMenu = document.getElementById('navbar_menu');
+  navbarMenu.classList.toggle('active');
+});
