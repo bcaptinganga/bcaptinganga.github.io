e42fddeffc7e56356639e79a423868cbe00c82d7
Our Lady Of The Holy Rosary Ting'ang'a <160110899+bcaptinganga@users.noreply.github.com>
2025-03-26 16:20:23 +0300

    Delete readings.js

--- readings.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('hamburger').addEventListener('click', function() {
-  const navbarMenu = document.getElementById('navbar_menu');
-  navbarMenu.classList.toggle('active');
-});
